65fc3eed7ce6a71f01d36d3ea239edcb5b86e7e6
Nanayawemperor <emmaokyere095@gmail.com>
2025-11-14 07:45:33 +0000

    robust error handling

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const inventoryRoute = require('./routes/inventoryRoute');
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const utilities = require('./utilities')
 
 /* ***********************
  *  View Engine and Templates
@@ -26,11 +27,29 @@ app.set('layout', './layouts/layout') //not at views root
 app.use(require('./routes/static'))
 
 //index route
-app.get('/', baseController.buildHome)
+app.get('/', utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use('/inv', inventoryRoute);
 app.use(static)
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
